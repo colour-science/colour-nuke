32127f54f5f9ec6b15da3c6a86108c0e81b463d8
Thomas Mansencal <thomas.mansencal@gmail.com>
2017-01-29 13:11:39 +1300

    Update copyright statements.

--- colour_nuke/lookup.py
+++ colour_nuke/lookup.py
@@ -21,7 +21,7 @@ import os
 from collections import namedtuple
 
 __author__ = 'Colour Developers'
-__copyright__ = 'Copyright (C) 2013-2016 - Colour Developers'
+__copyright__ = 'Copyright (C) 2013-2017 - Colour Developers'
 __license__ = 'New BSD License - http://opensource.org/licenses/BSD-3-Clause'
 __maintainer__ = 'Colour Developers'
 __email__ = 'colour-science@googlegroups.com'
